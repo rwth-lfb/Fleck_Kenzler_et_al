e1b3e394b7f263632149d27cccbc419b8b00b9b6
Martin Strauch <strauch@lfb.rwth-aachen.de>
2020-06-28 20:11:00 +0200

    documentation

--- demo.m
+++ demo.m
@@ -1,7 +1,9 @@
-%set paths:
-addpath(genpath('.\IAT_v0.9.3'));
-addpath('.\movement_correction');
-addpath('.\imaging_analysis');
+%code repository: https://github.com/rwth-lfb/Fleck_Kenzler_et_al
+%download and unzip the example movies: https://lfb.rwth-aachen.de/Fleck_Kenzler_et_al_data
+%see "manual.pdf" for instructions
+
+%get started by adding all required files to the Matlab path:
+set_path()
 
 %GUI for batch registration of recordings:
 movement_correction()
---
+++ set_path.m
@@ -0,0 +1,3 @@
+addpath(genpath('.\IAT_v0.9.3'));
+addpath('.\movement_correction');
+addpath('.\imaging_analysis');
