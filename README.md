88c8792860ddf09fd7da969fef6bf86c40441135
Martin Strauch <strauch@lfb.rwth-aachen.de>
2020-06-29 10:13:12 +0200

    links to example data

--- README.md
+++ README.md
@@ -2,7 +2,9 @@ source code for Fleck/Kenzler et al., 2020
 
 code: https://github.com/rwth-lfb/Fleck_Kenzler_et_al
 
-example data: https://lfb.rwth-aachen.de/Fleck_Kenzler_et_al_data
+example data:
+https://www.lfb.rwth-aachen.de/download/Fleck_Kenzler_et_al_data/calcium_imaging.zip
+https://www.lfb.rwth-aachen.de/download/Fleck_Kenzler_et_al_data/reflected_light_microscopy.zip
 
 instructions: demo.m, manual.pdf
 
--- demo.m
+++ demo.m
@@ -1,6 +1,9 @@
 %code repository: https://github.com/rwth-lfb/Fleck_Kenzler_et_al
-%download and unzip the example movies: https://lfb.rwth-aachen.de/Fleck_Kenzler_et_al_data
 %see "manual.pdf" for instructions
+
+%download and unzip the example movies: 
+%https://www.lfb.rwth-aachen.de/download/Fleck_Kenzler_et_al_data/calcium_imaging.zip
+%https://www.lfb.rwth-aachen.de/download/Fleck_Kenzler_et_al_data/reflected_light_microscopy.zip
 
 %get started by adding all required files to the Matlab path:
 set_path()
